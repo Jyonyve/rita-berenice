233196964848601dc0d784b2e2bc395e6933148a
Jyonyve <nthpopuptown@gmail.com>
2025-07-17 10:35:26 +0900

    style: slide with mouse

--- src/client/layout/glass/GlassPortraitSlider.tsx
+++ src/client/layout/glass/GlassPortraitSlider.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Pagination, A11y, Autoplay, EffectFade } from 'swiper/modules';
+import { Navigation, Pagination, A11y, Autoplay, Mousewheel, EffectFade } from 'swiper/modules';
 import { Box } from '@mui/material';
 import { GlassPortrait } from './GlassPortrait.js';
 
@@ -24,16 +24,22 @@ export const GlassPortraitSlider: React.FC<GlassPortraitSliderProps> = ({ imageU
 	return (
 		<Box sx={{ width: '100%', height: '100%', overflow: 'visible' }}>
 			<Swiper
-				modules={[Pagination, A11y, EffectFade]}
+				modules={[Pagination, A11y, EffectFade, Mousewheel]}
 				slidesPerView={1}
 				loop={true}
 				effect="fade"
 				fadeEffect={{ crossFade: true }}
 				style={{ overflow: 'visible' }}
 				pagination={{ clickable: true }}
+				mousewheel={{
+					forceToAxis: true, // Only respond to horizontal scroll
+					sensitivity: 1, // Adjust scroll sensitivity
+					releaseOnEdges: true, // Allow page scroll when at first/last slide
+					invert: true, // This inverts the scroll direction
+				}}
 			>
 				{imageUrls.map((url, index) => (
-					<SwiperSlide key={index} >
+					<SwiperSlide key={index}>
 						<GlassPortrait imageUrl={url} alt={`Character Portrait ${index + 1}`} />
 					</SwiperSlide>
 				))}
